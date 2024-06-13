cc046a3ec5c32635094c27410510e5cfed4b1f12
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-06-14 06:07:50 +0700

    biasa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../src/Ui/Navbar/Navbar"
+import About from "../src/Ui/Content/About"
 
 export default function App() {
   return (
     <div>
       <Navbar/>
+      <About/>
     </div>
   )
 }
