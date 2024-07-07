9d97cf3fd1c5faa77bc2f4a2f9b693e183c43601
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-07-07 07:32:47 +0700

    92%

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import Navbar from "../src/Ui/Components/Navbar/Navbar"
-import Home from "../src/Ui/Containers/Content/Home"
-import About from "../src/Ui/Containers/About/About"
-import Education from "../src/Ui/Containers/Education/Edukasi"
-import Serti from "../src/Ui/Containers/Certificates/Sertifikat"
-import Porto from "../src/Ui/Containers/Portfolio/Portfolio"
-import Skil from "../src/Ui/Containers/Skills/Skil"
-import Contact from "../src/Ui/Containers/Contact/Contact"
+import Navbar from "../src/Ui/Components/Navbar/Navbar";
+import Home from "../src/Ui/Containers/Content/Home";
+import About from "../src/Ui/Containers/About/About";
+import Education from "../src/Ui/Containers/Education/Edukasi";
+import Serti from "../src/Ui/Containers/Certificates/Sertifikat";
+import Porto from "../src/Ui/Containers/Portfolio/Portfolio";
+import Skil from "../src/Ui/Containers/Skills/Skil";
+import Contact from "../src/Ui/Containers/Contact/Contact";
+import Footer from "../src/Ui/Components/Footer/Footer";
 
 export default function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <About id='about'/>
-      <Skil id='skils'/>
-      <Education id='Edukasi'/>
-      <Serti id='serti'/>
-      <Porto id='portfolio'/>
-      <Contact id='contact'/>
+      <Navbar />
+      <Home />
+      <About id="about" />
+      <Skil id="skils" />
+      <Education id="Edukasi" />
+      <Serti id="serti" />
+      <Porto id="portfolio" />
+      <Contact id="contact" />
+      <Footer />
     </div>
-  )
+  );
 }
--- src/Ui/Containers/Contact/Contact.jsx
+++ src/Ui/Containers/Contact/Contact.jsx
@@ -1,46 +1,53 @@
 export default function Contact(props) {
-  const id = props.id;
-
-  return (
-    <div
-      id={id}
-      className="min-h-screen flex flex-col items-center justify-center"
-    >
-      <div className="bg-white text-black rounded-lg shadow-lg p-8 w-full max-w-lg">
-        <h2 className="text-2xl font-bold mb-6 text-center">Contact Me</h2>
-        <form className="space-y-4">
-          <input
-            type="text"
-            placeholder="Full Name"
-            className="w-full p-3 border border-gray-300 rounded-lg"
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full p-3 border border-gray-300 rounded-lg"
-          />
-          <input
-            type="tel"
-            placeholder="Mobile No."
-            className="w-full p-3 border border-gray-300 rounded-lg"
-          />
-          <input
-            type="text"
-            placeholder="Subject"
-            className="w-full p-3 border border-gray-300 rounded-lg"
-          />
-          <textarea
-            placeholder="Message"
-            className="w-full p-3 border border-gray-300 rounded-lg"
-          ></textarea>
-          <button
-            type="submit"
-            className="w-full bg-yellow-500 text-black p-3 rounded-lg font-bold"
-          >
-            Send
-          </button>
-        </form>
+    const id = props.id;
+  
+    return (
+      <div
+        id={id}
+        className="min-h-screen flex md:mt-4 mt-2 flex-col items-center justify-center"
+      >
+        <div className="bg-gray-100 rounded-3xl shadow-lg p-8 w-full max-w-max">
+          <h2 className="text-2xl text-yellow-600 font-bold mb-6 text-center">Contact Me</h2>
+          <form className="space-y-4">
+            <div className="md:flex md:space-x-4">
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="w-full md:w-1/2 p-3 border border-gray-300 rounded-lg mb-4 md:mb-0"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-full md:w-1/2 p-3 border border-gray-300 rounded-lg"
+              />
+            </div>
+            <div className="md:flex md:space-x-4">
+              <input
+                type="tel"
+                placeholder="Mobile No."
+                className="w-full md:w-1/2 p-3 border border-gray-300 rounded-lg mb-4 md:mb-0"
+              />
+              <input
+                type="text"
+                placeholder="Subject"
+                className="w-full md:w-1/2 p-3 border border-gray-300 rounded-lg"
+              />
+            </div>
+            <textarea
+              placeholder="Message"
+              className="w-full p-3 border border-gray-300 rounded-lg"
+            ></textarea>
+            <a href="mailto:mrafi.syahbani@gmail.com">
+            <button
+              type="submit"
+              className="w-full bg-yellow-500 text-black p-3 rounded-lg font-bold"
+            >
+              Send
+            </button>
+            </a>
+          </form>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
