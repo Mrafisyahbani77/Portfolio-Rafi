39d8c28db6518ff81089202f07505b040979225f
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-07-10 13:53:31 +0700

    oeee

--- src/Ui/Containers/About/About.jsx
+++ src/Ui/Containers/About/About.jsx
@@ -1,48 +1,72 @@
 import profileImage from "../../../assets/Images/pipi.png";
+import Rafi from "../../../assets/Images/vivi.png";
+import Lol from "../../../assets/Images/lol.png";
+import Rapi from "../../../assets/Images/pi.png";
 import bbl from "../../../assets/Images/bbl.png";
 import { AiFillSpotify } from "react-icons/ai";
 import { PiGameControllerDuotone } from "react-icons/pi";
 import { BsCalendar2Month } from "react-icons/bs";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Make sure this import is correct
+import { useEffect } from "react";
 
 const love = [
   { name: "Gaming", logo: PiGameControllerDuotone, isIcon: true },
   { name: "Workout", logo: bbl, isIcon: false },
   { name: "Music", logo: AiFillSpotify, isIcon: true },
-  { name: "24 AGUSTUS", logo: BsCalendar2Month, isIcon: true }
+  { name: "24 AGUSTUS", logo: BsCalendar2Month, isIcon: true },
 ];
 
 export default function About(props) {
   const id = props.id;
 
+  useEffect(() => {
+    console.log("Initializing AOS");
+    AOS.init({
+      duration: 1000, // Animation duration
+      once: true, // Whether animation should happen only once
+    });
+  }, []);
+
   return (
-    <main className="p-4 relative">
-      <div className="mt-28 text-center text-yellow-600 font-semibold text-3xl" id={id}>
+    <main className="p-4 mt-24 mb-24 relative">
+      <div
+        className="mt-28 text-center text-yellow-600 font-semibold text-3xl"
+        id={id}
+      >
         About Me
       </div>
       <div className="flex flex-col lg:flex-row justify-center items-center mt-8 lg:space-x-8">
         <div className="mx-3 relative text-center">
           <img
-            className="w-48 h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 rounded-full mb-4" // Increased size and added margin bottom
-            src={profileImage}
+            data-aos="flip-left"
+            className="w-32 h-32 md:w-52 md:h-52 lg:w-54 lg:h-54 rounded-full mb-4" // Increased size and added margin bottom
+            src={Lol}
             alt="Profile"
           />
           {love.map((fav, index) => {
-            const positionClass = index % 2 === 0 ? 'left-16' : 'right-14'; // Adjusted spacing
-            const verticalClass = index < 2 ? 'top-1/4' : 'top-2/3';
+            const positionClass = index % 2 === 0 ? "left-14" : "right-14"; // Adjusted spacing
+            const verticalClass = index < 2 ? "top-1/4" : "top-2/3";
 
             return (
-              <div key={index} className={`absolute ${positionClass} ${verticalClass} transform ${index % 2 === 0 ? '-translate-x-24' : 'translate-x-24'} mt-4`}>
-                <section className="flex items-center p-2 bg-slate-200 shadow-lg rounded-full animate-float">
+              <div
+              
+                key={index}
+                className={`absolute ${positionClass} ${verticalClass} transform ${
+                  index % 2 === 0 ? "-translate-x-28" : "translate-x-28"
+                } mt-4`}
+              >
+                <section className="flex items-center p-2 md:p-1 bg-slate-200 shadow-lg rounded-full animate-float">
                   {fav.isIcon ? (
-                    <fav.logo className="w-4 h-4 text-black md:w-6 md:h-6 mx-1" />
+                    <fav.logo className="w-3 h-3 text-black md:w-6 md:h-6 mx-1" />
                   ) : (
                     <img
-                      className="w-4 h-4 md:w-6 md:h-6 mx-1"
+                      className="w-3 h-3 md:w-6 md:h-6 mx-1"
                       src={fav.logo}
                       alt={fav.name}
                     />
                   )}
-                  <span className="text-xs md:text-sm text-black font-medium mr-2 md:mr-4">
+                  <span className="text-xs md:text-sm text-black font-semibold mr-2 md:mr-4">
                     {fav.name}
                   </span>
                 </section>
@@ -52,7 +76,10 @@ export default function About(props) {
         </div>
         <div className="mt-5 lg:mt-0 px-3 py-2 mx-3 lg:mx-0 lg:w-1/2 xl:w-1/3 flex justify-center bg-slate-300 rounded-2xl">
           <article className="font-normal text-black">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos quibusdam incidunt omnis voluptatem sed delectus esse maxime pariatur officia. Voluptate, cupiditate. Molestias architecto ea nesciunt, iusto deleniti unde omnis tenetur.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+            quibusdam incidunt omnis voluptatem sed delectus esse maxime
+            pariatur officia. Voluptate, cupiditate. Molestias architecto ea
+            nesciunt, iusto deleniti unde omnis tenetur.
           </article>
         </div>
       </div>
--- src/Ui/Containers/Skills/Skil.jsx
+++ src/Ui/Containers/Skills/Skil.jsx
@@ -1,40 +1,56 @@
 import React from "react";
 import { SiNextdotjs, SiPostman } from "react-icons/si";
-import Tailwind from "../../../assets/Images/tailwind.png";  
-import Html from "../../../assets/Images/html.png";  
-import Rea from "../../../assets/Images/Rea.png";  
-import Css from "../../../assets/Images/css.png";  
-import Js from "../../../assets/Images/javascript.png";  
+import Tailwind from "../../../assets/Images/tailwind.png";
+import Html from "../../../assets/Images/html.png";
+import Rea from "../../../assets/Images/Rea.png";
+import Css from "../../../assets/Images/css.png";
+import Js from "../../../assets/Images/javascript.png";
+import Vscode from "../../../assets/Images/vscode.png";
 
 const skills = {
-  designTools: [
-    { name: "Adobe XD", icon: "path-to-icon" },
-    { name: "Figma", icon: "path-to-icon" },
-  ],
+  // designTools: [
+  //   { name: "Adobe XD", icon: "path-to-icon" },
+  //   { name: "Figma", icon: "path-to-icon" },
+  // ],
   technologies: [
-    { name: "HTML5", icon:  <img src={Html} alt="Html" className="w-6 h-6" /> },
-    { name: "CSS3", icon:  <img src={Css} alt="CSS" className="w-6 h-6" /> },
-    { name: "JavaScript", icon:  <img src={Js} alt="Javascript" className="w-6 h-6" /> },
-    { name: "ReactJS", icon:  <img src={Rea} alt="React" className="w-6 h-6" /> },
+    { name: "HTML5", icon: <img src={Html} alt="Html" className="w-6 h-6" /> },
+    { name: "CSS3", icon: <img src={Css} alt="CSS" className="w-6 h-6" /> },
+    {
+      name: "JavaScript",
+      icon: <img src={Js} alt="Javascript" className="w-6 h-6" />,
+    },
+    {
+      name: "ReactJS",
+      icon: <img src={Rea} alt="React" className="w-6 h-6" />,
+    },
     { name: "NextJS", icon: <SiNextdotjs /> },
-    { name: "Tailwind CSS", icon: <img src={Tailwind} alt="Tailwind CSS" className="w-6 h-6" /> },
+    {
+      name: "Tailwind CSS",
+      icon: <img src={Tailwind} alt="Tailwind CSS" className="w-6 h-6" />,
+    },
   ],
-  tools: [
-    { name: "Visual Studio Code", icon: "path-to-icon" },
-    { name: "Github", icon: "path-to-icon" },
-    { name: "Notion", icon: "path-to-icon" },
-    { name: "Postman", icon: <SiPostman /> },
-  ],
+  // tools: [
+  //   {
+  //     name: "Visual Studio Code",
+  //     icon: <img src={Vscode} alt="Tailwind CSS" className="w-9 h-5" />,
+  //   },
+  //   { name: "Github", icon: "path-to-icon" },
+  //   { name: "Notion", icon: "path-to-icon" },
+  //   { name: "Postman", icon: <SiPostman /> },
+  // ],
 };
 
 export default function Skil(props) {
   const id = props.id;
 
   return (
-    <div id={id} className="min-h-screen text-black flex flex-col items-center py-10">
+    <div
+      id={id}
+      className="mb-20 mt-8 text-black flex flex-col items-center py-10"
+    >
       <h1 className="text-4xl text-yellow-600 font-bold mb-14">Skills</h1>
 
-      <section className="mb-10 w-4/5 md:w-2/3 lg:w-1/2">
+      {/* <section className="mb-10 w-4/5 md:w-2/3 lg:w-1/2">
         <h2 className="text-2xl text-center text-yellow-600 font-semibold mb-4">Design Tools I Use</h2>
         <div className="flex justify-center space-x-4">
           {skills.designTools.map((tool, index) => (
@@ -47,10 +63,9 @@ export default function Skil(props) {
             </div>
           ))}
         </div>
-      </section>
+      </section> */}
 
-      <section className="mb-10 w-4/5 md:w-2/3 lg:w-1/2">
-        <h2 className="text-2xl text-yellow-600 font-semibold text-center mb-4">Technologies I Use</h2>
+      <section className="w-4/5 md:w-2/3 lg:w-1/2">
         <div className="flex flex-wrap justify-center gap-4">
           {skills.technologies.map((tech, index) => (
             <div
@@ -64,7 +79,7 @@ export default function Skil(props) {
         </div>
       </section>
 
-      <section className="mb-10 w-4/5 md:w-2/3 lg:w-1/2">
+      {/* <section className="mb-10 w-4/5 md:w-2/3 lg:w-1/2">
         <h2 className="text-2xl text-yellow-600 text-center font-semibold mb-4">
           Development & Productivity Tools I Use
         </h2>
@@ -79,7 +94,7 @@ export default function Skil(props) {
             </div>
           ))}
         </div>
-      </section>
+      </section> */}
     </div>
   );
 }
